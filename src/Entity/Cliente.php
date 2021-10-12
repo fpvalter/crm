111f176e718b3c7a4c2850985de5e49dd69b8b5d
Valter Fernandes <valter@digi4ind.com>
2021-10-12 11:54:03 -0300

    Added API Platform

--- src/Entity/Cliente.php
+++ src/Entity/Cliente.php
@@ -2,13 +2,18 @@
 
 namespace App\Entity;
 
-use App\Repository\ClienteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ClienteRepository::class)
+ * 
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "put"}
+ * )
  */
 class Cliente
 {
